0f9ec9ac46e83c07f2aa5de19c626113a049892b
65656567 <robersonkellan@gmail.com>
2025-09-15 13:26:42 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,6 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/services" element={<Services />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
