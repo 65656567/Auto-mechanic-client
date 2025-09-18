40234b0fbde8decbba2ba006b2951e485c50e4a2
65656567 <robersonkellan@gmail.com>
2025-09-18 16:31:12 -0600

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Phone } from "lucide-react";
-import heroImage from "@/assets/image(1).jpg";
+import heroImage from "@/assets/image.jpg";
 
 const HeroSection = () => {
   return (
